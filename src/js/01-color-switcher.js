66d54e689285a3a6144e75d7060e3760a8e28a5f
Sunwart <li.sa.vectorart@gmail.com>
2021-11-05 20:04:11 +0200

    Task 1 completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+import { getRandomHexColor } from './random-color';
 
+const startBtnElem = document.querySelector('button[data-start]');
+const stopBtnElem = document.querySelector('button[data-stop]');
+stopBtnElem.disabled = true;
+
+startBtnElem.addEventListener('click', startChangingBgColor);
+stopBtnElem.addEventListener('click', stopChangingBgColor);
+
+let timerId;
+
+function startChangingBgColor(event) {
+  timerId = setInterval(changeBodyColor, 1000);
+  startBtnElem.disabled = true;
+  stopBtnElem.disabled = false;
+}
+
+function stopChangingBgColor(event) {
+  clearInterval(timerId);
+  startBtnElem.disabled = false;
+  stopBtnElem.disabled = true;
+}
+
+function changeBodyColor() {
+  document.querySelector('body').style.backgroundColor = getRandomHexColor();
+}
